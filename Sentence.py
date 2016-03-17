dbaa0982a483fa93ee002d7a4b8011244885ab17
JerEpoch <jersoc@gmail.com>
2016-03-17 14:12:26 -0500

    Added files via upload

---
+++ Sentence.py
@@ -0,0 +1,9 @@
+
+#just prints a sentence based on user input
+
+animal=input('Enter an animal: ')
+verb=input('Enter an action for the animal: ')
+adverb=input('Enter an adverb: ')
+
+print('The', animal, 'is', verb, adverb)
+
